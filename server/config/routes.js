5bde722364b4076865228d91b6d654eec1d6416d
Alec Maliwanag <amaliwanag@gmail.com>
2016-11-12 15:05:24 -0800

    (clean up) (test) connect user db to server (#24)
    * Implement user/concoction models and user controller.
    
    * (feat) routing skeleton complete (#20)
    
    * (feat) started building routing
    
    * (feat) routing skeletons complete
    
    * (fix) added path as dependency
    
    * Implement sign in/ log in on user controller and password hashing.
    
    * Setup foundation for authentication and writing to DB.
    
    * (cleanup) (test) Clean up user controller and connect user DB to server.
    
    * Slight debug.

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,9 +1,7 @@
 const concoctionConstructor = require('./routers/constructor');
 const userRouter = require('./routers/user');
-// put routers here
-
-
 module.exports = (app) => {
-  app.use('/api/constructor', concoctionConstructor);
+  // app.use('/api/constructor', concoctionConstructor);
   app.use('/api/user', userRouter);
+  // app.use('/api/oauth', oauthRouter);
 };
