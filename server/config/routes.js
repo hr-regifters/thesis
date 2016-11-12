b6448bb57657ef1fc4f0ad3f20d1e4e895f7dcb0
Ben Hesseldieck <b.hesseldieck@gmail.com>
2016-11-11 19:35:37 -0800

    (fix) changed port and dummy function for constructor (#22)
    * (feat) started building routing
    
    * (feat) routing skeletons complete
    
    * (fix) corrected port and dummy function in constructor

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,5 +1,7 @@
 const concoctionConstructor = require('./routers/constructor');
 const userRouter = require('./routers/user');
+// put routers here
+
 
 module.exports = (app) => {
   app.use('/api/constructor', concoctionConstructor);
