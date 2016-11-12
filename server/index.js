da10cac7f75ee9d16aed54d1812e96d1fdb107de
BHesseldieck <b.hesseldieck@gmail.com>
2016-11-11 19:28:39 -0800

    (fix) corrected port and dummy function in constructor

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 1337;
 
 require('./db/config.js');
 require('./config/middleware.js')(app, express);
