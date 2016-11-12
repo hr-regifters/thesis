9a9043111eded943ac9d3a096bc220eeb02fc2f2
Alec Maliwanag <amaliwanag@gmail.com>
2016-11-11 20:04:11 -0800

    Setup foundation for authentication and writing to DB.

---
+++ server/db/controllers/slackController.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const passport = require('passport');
+const SlackStrategy = require('passport-slack').Strategy; 
+const clientID = require('../../env.js').clientID;
+const clientSecret = require('../../env.js').clientSecret;
+
+passport.use(new SlackStrategy({
+    clientID: clientID,
+    clientSecret: clientSecret
+  },
+  function(accessToken, refreshToken, profile, done) {
+    User.findOrCreate({ SlackId: profile.id }, function (err, user) {
+      return done(err, user);
+    });
+  }
+));
