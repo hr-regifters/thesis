0381b913f176438b809d09082283845183643f2f
Ben Hesseldieck <b.hesseldieck@gmail.com>
2016-11-11 19:20:14 -0800

    (feat) routing skeleton complete (#20)
    * (feat) started building routing
    
    * (feat) routing skeletons complete
    
    * (fix) added path as dependency

---
+++ server/config/routes.js
@@ -0,0 +1,7 @@
+const concoctionConstructor = require('./routers/constructor');
+const userRouter = require('./routers/user');
+
+module.exports = (app) => {
+  app.use('/api/constructor', concoctionConstructor);
+  app.use('/api/user', userRouter);
+};
--- server/index.js
+++ server/index.js
@@ -1,2 +1,15 @@
 const express = require('express');
+
 const app = express();
+
+const port = process.env.PORT || 3000;
+
+require('./db/config.js');
+require('./config/middleware.js')(app, express);
+require('./config/routes.js')(app);
+
+app.listen(port, () => {
+  console.log(`Server listens on Port ${port}`);
+});
+
+module.exports = app;
