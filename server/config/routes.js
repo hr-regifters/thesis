acd4973d933f8f28481cc1e37a52646ee8c34844
BHesseldieck <b.hesseldieck@gmail.com>
2016-11-11 18:52:59 -0800

    (feat) started building routing

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,7 +1,9 @@
 const concoctionConstructor = require('./routers/constructor');
 const userRouter = require('./routers/user');
+// put controllers here
 
 module.exports = (app) => {
   app.use('/api/constructor', concoctionConstructor);
   app.use('/api/user', userRouter);
 };
+
