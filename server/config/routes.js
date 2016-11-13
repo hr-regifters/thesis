0a5693619a0265be617ba0cadc50090195d423e6
BHesseldieck <b.hesseldieck@gmail.com>
2016-11-12 18:56:11 -0800

    (feat) bug with calling actions solved

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,12 +1,12 @@
-const concoctionConstructor = require('./routers/constructor');
-const userRouter = require('./routers/user');
-const oauthRouter = require('./routers/oauth');
+// const concoctionConstructor = require('./routers/constructor');
+// const userRouter = require('./routers/user');
+// const oauthRouter = require('./routers/oauth');
 const webhookRouter = require('./routers/webhooks');
 // put routers here
 
 module.exports = (app) => {
-  app.use('/api/constructor', concoctionConstructor);
-  app.use('/api/user', userRouter);
-  app.use('/api/oauth', oauthRouter);
+  // app.use('/api/constructor', concoctionConstructor);
+  // app.use('/api/user', userRouter);
+  // app.use('/api/oauth', oauthRouter);
   app.use('/api/webhooks', webhookRouter);
 };
--- server/config/webhooksHandlers/apiHandlers/slack.js
+++ server/config/webhooksHandlers/apiHandlers/slack.js
@@ -1,10 +1,9 @@
 const async = require('async');
-const webhooksHandler = require('./../main');
-const evernote = require('./evernote');
 // const slackCollection = require('./../../../db/models/slackModel')
 
 module.exports = {
   trigger: (req, res) => {
+    const webhooksHandler = require('./../main');
     if (req.body.type === 'url_verification') {
       res.json({ challenge: req.body.challenge });
     } else {
@@ -14,7 +13,9 @@ module.exports = {
     // fetch db data for users to get actions
     // extract actions if trigger is right
     console.log(webhooksHandler);
-    webhooksHandler['evernoteAction']['post']('hello from slack event');
+    async.parallel([webhooksHandler['evernoteAction']['post'], webhooksHandler['evernoteAction']['delete']], (err, results) => {
+      console.log('results: ', results);
+    });
     // use async.parallel webhooksHandler[api + Action][action](parameters) to shoot the actions
   },
   actions: {
