ca3dd7b1dbb405a32f4c65bf236d4c14938be8f2
Alec Maliwanag <amaliwanag@gmail.com>
2016-11-14 12:22:12 -0800

    (feat) Add model for slack trigger collection.

---
+++ server/db/models/slackTriggerModel.js
@@ -0,0 +1,21 @@
+const mongoose = require('../config.js');
+const Schema = mongoose.Schema;
+
+//solution currently only handles 1 action for 1 trigger
+const slackTriggerSchema = new Schema({
+  trigger: {
+    type: String,
+    index: true,
+    unique: true
+  },
+  action: [{
+    slackUserId: String,
+    actionApi: String,
+    actionKey: String,
+    actionFunctions: String,
+    actionParams: [{}],
+  }]
+  
+});
+
+module.exports = mongoose.model('slackTrigger', slackTriggerSchema);
