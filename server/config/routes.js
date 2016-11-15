d34ff42ef228e6610235b72c08dc5a508e9c344b
Alec Maliwanag <amaliwanag@gmail.com>
2016-11-15 15:22:18 -0800

    (cleanup) (test) Clean up user controller and connect user DB to server.

--- server/config/routes.js
+++ server/config/routes.js
@@ -5,8 +5,7 @@ const webhookRouter = require('./routers/webhooks');
 // put routers here
 
 module.exports = (app) => {
-  // app.use('/api/constructor', concoctionConstructor);
-  // app.use('/api/user', userRouter);
-  // app.use('/api/oauth', oauthRouter);
-  app.use('/api/webhooks', webhookRouter);
-};
+  app.use('/api/constructor', concoctionConstructor);
+  app.use('/api/user', userRouter);
+  app.use('/api/oauth', oauthRouter);
+}
