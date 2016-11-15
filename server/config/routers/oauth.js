9c12a3fe9202e556f41dc693c2bd202254eb9592
Kyle Chun <kbchun@users.noreply.github.com>
2016-11-15 09:29:10 -0800

    (feat) Implement Evernote OAuth and refactor OAuth strategies/routes (#31)
    * (fix) Fix bug regarding OAuth
    
    * (fix) Fix alec's mistakes
    
    * Retrieve user's slack token and save to DB
    
    * Implement Evernote OAuth and refactor OAuth strategies/routes

--- server/config/routers/oauth.js
+++ server/config/routers/oauth.js
@@ -1,11 +1,21 @@
 const express = require('express');
+const passport = require('passport');
 const SlackStrategy = require('../../db/controllers/slackController');
 const router = new express.Router();
 
-router.get('/slack', SlackStrategy.authenticate('slack'));
+router.get('/slack', passport.authenticate('slack'));
 
 router.get('/slack/callback',
-  SlackStrategy.authorize('slack', { failureRedirect: '/login' }),
+  passport.authorize('slack', { failureRedirect: '/login' }),
+  function(req, res) {
+    res.redirect('/');
+  }
+);
+
+router.get('/evernote', passport.authenticate('evernote'));
+
+router.get('/evernote/callback', 
+  passport.authenticate('evernote', { failureRedirect: '/login' }),
   function(req, res) {
     res.redirect('/');
   }
--- server/db/controllers/slackController.js
+++ server/db/controllers/slackController.js
@@ -1,17 +1,19 @@
-const express = require('express');
 const passport = require('passport');
-const SlackStrategy = require('passport-slack').Strategy;
-const CLIENT_ID = require('../../../env.js').CLIENT_ID;
-const CLIENT_SECRET = require('../../../env.js').CLIENT_SECRET;
 const utility = require('./userController');
 
-module.exports = passport.use(new SlackStrategy({
-  clientID: CLIENT_ID,
-  clientSecret: CLIENT_SECRET,
+const SlackStrategy = require('passport-slack').Strategy;
+const SLACK_ID = require('../../../env.js').SLACK_ID;
+const SLACK_SECRET = require('../../../env.js').SLACK_SECRET;
+
+module.exports = new SlackStrategy({
+  clientID: SLACK_ID,
+  clientSecret: SLACK_SECRET,
+  callbackURL: 'http://127.0.0.1:1337/api/oauth/slack/callback',
   scope: 'incoming-webhook users:read'
-},
-function(accessToken, refreshToken, profile, done) {
-  utility.addToken('alec', 'slackToken', accessToken);
-  return done(null, profile);
-}
-));
+}, (accessToken, refreshToken, profile, done) => {
+  process.nextTick(() => {
+    console.log(accessToken, profile);
+    // utility.addToken('alec', 'evernoteToken', accessToken);
+    return done(null, profile);
+  });
+});
