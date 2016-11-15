c96f4e98d3a2e0b7a0f05d07a04400ae2e5f0968
BHesseldieck <b.hesseldieck@gmail.com>
2016-11-14 17:35:17 -0800

    (feat) trigger calling action in parallel

--- server/config/webhooksHandlers/apiHandlers/slack.js
+++ server/config/webhooksHandlers/apiHandlers/slack.js
@@ -1,5 +1,6 @@
 const async = require('async');
-// const slackCollection = require('./../../../db/models/slackModel')
+// const slackCtrl = require('./../../../db/models/slack');
+// const slackCollection = require('./../../../db/models/slackModel');
 
 module.exports = {
   trigger: (req, res) => {
@@ -9,13 +10,26 @@ module.exports = {
     } else {
       res.status(200).send('registered slack event');
     }
-    console.log('shoot actions after slack trigger');
     // fetch db data for users to get actions
+    // slackCtrl.getEvent(req.body.event.type).then((arr) => {
+      arr = [{api: 'evernote', action:'post', slackId: 'U061F7AUR', params: 'post this shit 1'},
+             {api: 'evernote', action:'delete', slackId: 'U061F7AUR', params: 'delete first 2'},
+             {api: 'evernote', action:'post', slackId: 'U061FAUR', params: 'shouldnt occur'}];
+      async.each(arr, (obj, callback) => {
+        if (req.body['authed_users'].includes(obj.slackId)) {
+          if (obj.api === undefined || obj.action === undefined) {
+            callback('error! api or action property not existing');
+          } else {
+            webhooksHandler[`${obj.api}Action`][obj.action](obj.params);
+            callback();
+          }
+        } else {
+          callback();
+        }
+      }, (error) => { error !== null ? console.log(error) : console.log('slack event successfully processed'); });
+    // }).catch((error) => { console.log(error); });
+
     // extract actions if trigger is right
-    console.log(webhooksHandler);
-    async.parallel([webhooksHandler['evernoteAction']['post'], webhooksHandler['evernoteAction']['delete']], (err, results) => {
-      console.log('results: ', results);
-    });
     // use async.parallel webhooksHandler[api + Action][action](parameters) to shoot the actions
   },
   actions: {
