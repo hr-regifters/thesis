40fd8bca0a3b1fe37f0e8ecf60db10f089425363
Dylan Larrabee <dylanlarrabee6@gmail.com>
2016-11-18 15:37:19 -0800

    Add more functionality to client (#56)
    * Initial commit
    
    * Lay out needed information in app state
    
    * Add skeleton of HomeView
    
    * Start on concoction editing page
    
    * Add webPack dependencies
    
    * Made gitignore ignore modules in client
    
    * Add Add concoction skeleton render
    
    * Remove node modules
    
    * Fix syntax errors
    
    * Fix indentation error
    
    * Modify gitignore
    
    * Add syntax fixes
    
    * Add scripts to test writing notes to evernote
    
    * Test write too a specific notebook
    
    * Add tagging functionality to notes scripts
    
    * Parse the req from the webbhook event
    
    * Add timestamp to note body
    
    * Add functionality to evennote postNote action
    
    * Update skeleton of services detail json
    
    * Use services detail to render options in client
    
    * Modify Trigger component
    
    * Clean up pull request
    
    * Complete skeleton of trigger select functionality
    
    Needs to be styled, but it saves selections into the addConcoctionView's state to be bundled and sent to the server when the save button is pressed
    
    * Add dynamic rendering and adding of action bttns
    
    * Change rendering of bttn on completeness of state
    
    * Manage more merge conflicts
    
    * Remove file from index.html
    
    * Remove uneeded clause in else statement

--- client/src/components/Action.jsx
+++ client/src/components/Action.jsx
@@ -1,15 +1,100 @@
 import React from 'react';
+//require('../../dist/main.css');
 
-export default class Action extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
+const Action = (props) => {
+  
+  if (props.state.action === '') {
+  //   //display slack and evernote triggers
+    return(
       <div>
-      Action
+        <div onClick={ () => {props.modifyActionReveal(props.actionsIndex)}}>
+        Action:
+        </div>
+        <div className={props.state.actionServicesReveal}>
+          {Object.keys(props.servicesDetail.servicesDetailJSON).map(function(service) {
+            return (
+              <div onClick={ () => {props.modifyAction(service, props.actionsIndex)}}>
+              {service}
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    );
+  } else if (props.state.action !== '' && props.state.actionOption === '') {
+    //display trigger in state and show options for trigger in state
+    return(
+      <div>
+        <div onClick={ () => {props.modifyActionReveal(props.actionsIndex)}}>
+        Action:  {props.servicesDetail.servicesDetailJSON[props.state.action].icon} {props.state.action}
+        <span onClick={ () => {props.modifyAction('', props.actionsIndex)}}>X</span>
+        </div>
+        <div className={props.state.actionServicesReveal}>
+          {props.servicesDetail.servicesDetailJSON[props.state.action].action.options.map(function(option, index){
+            return (
+              <div onClick={ () => {props.modifyActionOption(index, props.actionsIndex)}}>
+              {option.description}
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    );
+  } else if (props.state.action !== '' && props.state.actionOption !== '' && props.servicesDetail.servicesDetailJSON[props.state.action].action.options[props.state.actionOption].parameters.length === 0) {
+    //display trigger and option in state 
+    return(
+      <div>
+        <div onClick={ () => {props.modifyActionReveal(props.actionsIndex)}}>
+        Action: {props.servicesDetail.servicesDetailJSON[props.state.action].icon} {props.state.action}
+        </div>
+        <div className={props.state.actionServicesReveal}>
+          <div>
+          {props.servicesDetail.servicesDetailJSON[props.state.action].action.options[props.state.actionOption].description}
+          <span onClick={ () => {props.modifyTriggerOption('', props.actionsIndex)}}>X</span>
+          </div>
+          <div>
+          Save Action
+          </div>
+        </div>
+      </div>
+    );
+  } else if (props.state.action !== '' && props.state.actionOption !== '' && props.servicesDetail.servicesDetailJSON[props.state.action].action.options[props.state.actionOption].parameters.length > 0) {
+    //display trigger and option in state 
+    return(
+     <div>
+        <div onClick={ () => {props.modifyActionReveal(props.actionsIndex)}}>
+        Action: {props.servicesDetail.servicesDetailJSON[props.state.action].icon} {props.state.action}
+        </div>
+        <div className={props.state.actionServicesReveal}>
+          <div>
+          {props.servicesDetail.servicesDetailJSON[props.state.action].action.options[props.state.actionOption].description}
+          <span onClick={ () => {props.modifyActionOption('', props.actionsIndex)}}>X</span>
+          </div>
+          {props.servicesDetail.servicesDetailJSON[props.state.action].action.options[props.state.actionOption].parameters.map(function(param) {
+            return(
+              <div>
+              {param.description}: <input id='param' type='text' className={param.alias}></input>
+              </div>
+            )
+          })}
+          <div onClick={ () => {props.modifyActionParams(document.getElementById('param').value, document.getElementById('param').className, props.actionsIndex)}}>
+          Save Action
+          </div>
+        </div>
       </div>
     );
   }
-}
+
+};
+
+
+export default Action;
+
+// render params and save to state ----
+// change structure of trigger button to something prettier ----
+//make action component ----
+// make add action  component ----
+// render trigger, action, add action, and save components depending on completeness of state ----
+// style services based on services verified boolean
+// build and send the concoction bundle to the server
+// figure out service authenticating without entire app rerender
