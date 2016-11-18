e9dfdcc2faad5956d0e4412af73e5c7f4103d65f
Kyle Chun <kbchun5712@gmail.com>
2016-11-17 16:14:55 -0800

    (bug) Remove parse

--- server/db/controllers/slackController.js
+++ server/db/controllers/slackController.js
@@ -25,6 +25,6 @@ module.exports.getFile = (slackId, fileId) => {
     return request(`https://slack.com/api/files.info?token=${user.slackToken}&file=${fileId}&pretty=1`)
   })
   .then((fileObj) => {
-    return JSON.parse(fileObj).file;
+    return fileObj.file;
   });
 }
