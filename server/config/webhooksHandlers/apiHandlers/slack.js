6097280c3c50718b0b4b3e5299bb5a0218c8db1d
Ben Hesseldieck <b.hesseldieck@gmail.com>
2016-11-17 21:25:30 -0800

    (feat) Slack Webhook file_create ---> posts note with file-link or image to evernote  (#50)
    * (test) webhook logging
    
    * (test) testing event api of Slack
    
    * (feat) Node running on Cluster
    
    * (refactor) deleted unneeded comments
    
    * (fix) strict mode for oauth
    
    * (feat) implemented delayed worker restart when died
    
    * (test) better insight in event webhook
    
    * (test) testing webhook
    
    * (test) exploring file property
    
    * (test) back to logging full req.body
    
    * (test) listening for messages
    
    * (refactor) created Url file in env to quickly change url for local testing
    
    * (optimization) cluster just running if more than 1 worker possible
    
    * (feat) webhook --> action
    
    * (fix) authed not recognized as array
    
    * (bug) check if authed users is array
    
    * (test) test if authed users is array
    
    * (test) changed from includes to indexOf
    
    * (fix) undefined var noteDetails
    
    * (test) added more logging in evernote webhookhandler
    
    * (bug) undefined evernoteToken;
    
    * (feat) slack webhook file_create --> post note on evernote complete

--- server/config/webhooksHandlers/apiHandlers/slack.js
+++ server/config/webhooksHandlers/apiHandlers/slack.js
@@ -1,67 +1,63 @@
 "use strict"
 const async = require('async');
-// const slackCtrl = require('../../../db/models/slack');
 const concCtrl = require('../../../db/controllers/concoctionController');
 const slackCtrl = require('../../../db/controllers/slackController');
 
 module.exports = {
   trigger: (req, res) => {
     const webhooksHandler = require('./../main');
+    if (req.body.type === 'url_verification') {
+      res.json({ challenge: req.body.challenge });
+    } else {
+      res.status(200).send('registered slack event');
+      const slackId = req.body['authed_users'][0];
+      const fileId = req.body.event.file_id;
+      slackCtrl.getFile(slackId, fileId)
+      .then((file) => {
 
-    const slackId = req.body.authed_users[0];
-    const fileId = req.body.event.file_id;
-    slackCtrl.getFile(slackId, fileId)
-    .then((file) => {
-      console.log('FILE RECEIVED', file);
-      if (req.body.type === 'url_verification') {
-        res.json({ challenge: req.body.challenge });
-        return;
-      } else {
-        res.status(200).send('registered slack event');
-      }
+        let slackReqObj = {
+          slackUserId: '',
+          title: '',
+          body: '',
+          links: [],
+          images: [],
+          tagNames: [],
+          actionParams: '',
+        };
 
-      let slackReqObj = {
-        userId: '',
-        title: '',
-        body: '',
-        links: [],
-        images: [],
-        tagNames: [],
-        actionParams: '',
-      };
-      if (req.body.event.type === 'file_created') {
-        slackReqObj.title = file.title;
-        slackReqObj.images = [file.url_private];
-        slackReqObj.body = new Date(file.timestamp * 1000).toString();
-        slackReqObj.tagNames = ['Slack', 'Upload'];
-      }
-
-      console.log('SLACK OBJECT WOOOO', slackReqObj);
-
-      // fetch db data for users to get actions
-      concCtrl.getSlackEvent(req.body.event.type).then((arr) => {
-        // arr = [{actionApi: 'evernote', actionFunction:'post', slackUserId: 'U061F7AUR', actionParams: 'post this shit 1'},
-        //        {actionApi: 'evernote', actionFunction:'delete', slackUserId: 'U061F7AUR', actionParams: 'delete first 2'},
-        //        {actionApi: 'evernote', actionFunction:'post', slackUserId: 'U061FAUR', actionParams: 'shouldnt occur'}];
-        async.each(arr, (obj, callback) => {
-          if (req.body['authed_users'].includes(obj.slackUserId)) {
-            if (obj.actionApi === undefined || obj.actionFunction === undefined) {
-              callback('error! actionApi or actionFunction property not existing');
+        // fetch db data for users to get actions
+        concCtrl.getSlackEvent(req.body.event.type).then((arr) => {
+          async.each(arr, (obj, callback) => {
+            if (req.body['authed_users'].indexOf(obj.slackUserId) !== -1) {
+              if (obj.actionApi === undefined || obj.actionFunction === undefined) { // check for additional things like token, api_app_id, timestamp
+                callback('error! actionApi or actionFunction property not existing');
+              } else {
+                if (req.body.event.type === 'file_created' && obj.actionApi === 'evernote' && obj.actionFunction === 'postNote') {
+                  slackReqObj.title = file.title;
+                  if (file.mimetype.slice(0, 5) === 'image') {
+                    slackReqObj.images = [file.url_private];
+                  } else {
+                    slackReqObj.links = [file.url_private];
+                  }
+                  slackReqObj.body = new Date(file.timestamp * 1000).toString();
+                  slackReqObj.tagNames = ['Slack', 'Upload'];
+                }
+                slackReqObj.slackUserId = obj.slackUserId;
+                slackReqObj.actionParams = obj.actionParams;
+                webhooksHandler[`${obj.actionApi}Action`][obj.actionFunction](slackReqObj);
+                callback();
+              }
             } else {
-              slackReqObj.actionParams = obj.actionParams;
-              webhooksHandler[`${obj.actionApi}Action`][obj.actionFunction](slackReqObj);
               callback();
             }
-          } else {
-            callback();
-          }
-        }, (error) => { error ? console.log(error) : console.log('slack event successfully processed'); });
-      }).catch((error) => { console.log(error); });
+          }, (error) => { error ? console.log(error) : false; });
+        }).catch((error) => { console.log(error); });
 
-      // extract actions if trigger is right
-      // use async.parallel webhooksHandler[api + Action][action](parameters) to shoot the actions
+        // extract actions if trigger is right
+        // use async.parallel webhooksHandler[api + Action][action](parameters) to shoot the actions
 
-    });
+      });
+    }
   },
   actions: {
 
