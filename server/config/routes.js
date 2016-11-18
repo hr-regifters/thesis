723519787bd8be9a704955a69d75ae145bdab461
Ben Hesseldieck <b.hesseldieck@gmail.com>
2016-11-17 22:25:40 -0800

    (test) config loaderio

--- server/config/routes.js
+++ server/config/routes.js
@@ -9,6 +9,7 @@ module.exports = (app) => {
   app.use('/api/user', userRouter);
   app.use('/api/oauth', checkLogIn, oauthRouter);
   app.use('/api/webhooks', webhookRouter);
+  app.use('/loaderio-63505aef1c99acae4eea09e626d6e2fc/', (req, res) => { res.send('loaderio-63505aef1c99acae4eea09e626d6e2fc') });
 };
 
 const checkLogIn = function(req, res, next) {
