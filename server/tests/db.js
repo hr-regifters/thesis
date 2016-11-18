8cd38cb345f777c7b6d37ab97cc651b7be15727b
Alec Maliwanag <amaliwanag@gmail.com>
2016-11-18 09:24:36 -0800

    (test) Tests now test db.

--- server/tests/db.js
+++ server/tests/db.js
@@ -8,35 +8,22 @@ var Slack = require('../db/controllers/slackController');
 var request = require('request');
 var db = require('../db/config');
 
-describe('Kyle feeds top lane', function() {
+describe('It should write username, password, and email to the database', function() {
 // TODO edit this line if your database name is not "archive":
 var dbName = 'thesisdemo';
 var dbUrl = 'mongodb://localhost:27017/' + dbName;
 
 it('Should store requested documents in Mongo', function(done) {
 
-  /* TODO edit these variables to match the interface of your
-   * archive server. */
-  var archiveServer = 'http://127.0.0.1:1337/api/user/signup';
-  var archiveForm = {username: 'simba', password: 'littlemermaid', email: 'google.com'};
+  UserCTRL.signup({body:{username:'troll', password:'troll1', email:'troll2'}})
 
-  // Post a message to the archive server:
-  request({
-    method: 'POST',
-    uri: archiveServer,
-    form: archiveForm
-  }, function(error, response, body) {
-    /* Now if we look in the database, we should find the
-     * posted message there. */
+  // Wait a second for it to be done archiving
+  waits(1000);
 
-    // Wait a second for it to be done archiving
-    waits(1000);
-
-    runs(function() {
-      User.findOne({username: 'simba'}).then(function(user) {
-        expect(user.username).to.equal('simba');
-      })
+  runs(function() {
+    User.findOne({username: 'troll'}).then(function(user) {
+      expect(user.username).to.equal('troll');
+      expect(user.email).to.equal('troll2');
     })
   })
-})
 })
