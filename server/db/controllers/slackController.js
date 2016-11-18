c8545908874b513eb16e377b72987676f0d4be9f
Kyle Chun <kbchun5712@gmail.com>
2016-11-17 16:23:18 -0800

    (bug) Parse object

--- server/db/controllers/slackController.js
+++ server/db/controllers/slackController.js
@@ -25,7 +25,7 @@ module.exports.getFile = (slackId, fileId) => {
     return request(`https://slack.com/api/files.info?token=${user.slackToken}&file=${fileId}&pretty=1`)
   })
   .then((fileObj) => {
-    console.log(fileObj);
-    return fileObj.file;
+    console.log('TYPE OF', typeof fileObj);
+    return JSON.parse(fileObj).file;
   });
 }
