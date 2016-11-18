6f826dcbbf1f02d872b56cb7327892a42505e942
Dylan Larrabee <dylan.r.larrabee@gmail.com>
2016-11-18 15:29:43 -0800

    Manage more merge conflicts

--- client/src/components/SaveNewConcoction.jsx
+++ client/src/components/SaveNewConcoction.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
-import currUrl from './../../../currUrl';
 
 
-<<<<<<< HEAD
 const SaveNewConcoction = (props) => {
   if (!props.state.actions.reduce(function(prev, curr) {
       var complete = true;
@@ -17,52 +15,9 @@ const SaveNewConcoction = (props) => {
       </div>
     );
   } else {
-||||||| merged common ancestors
-export default class SaveNewConcoction extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-=======
-export default class SaveNewConcoction extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  saveConcoction() {
-    let context = this;
-    fetch(`${currUrl}/api/constructor/slack/add`, {
-      method: 'POST',
-      headers: {'Content-Type': 'application/json'},
-      body: JSON.stringify({
-        trigger: 'dummy data',
-        username: 'dummy data',
-        actionApi: 'dummy data',
-        actionFunction: 'dummy data',
-        actionParams: '{"dummy": "data"}' // parent notebook, evernote token, 
-      })
-    })
-    .then(function(res) {
-      if (res.status === 201) {
-        context.props.changeViewTo('home');
-      }
-    });
-  }
-
-  render() {
->>>>>>> 86085e43dc99c42e32cb718de662b88d60601790
     return (
-<<<<<<< HEAD
       <div className='saveEnabled'>
       Save New Concoction
-||||||| merged common ancestors
-      <div>
-     Save
-=======
-      <div>
-        <button className="saveButton" onClick={this.saveConcoction.bind(this)} type="button">Save</button>
->>>>>>> 86085e43dc99c42e32cb718de662b88d60601790
       </div>
     );
   }
