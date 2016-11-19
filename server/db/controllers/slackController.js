851e8ee82e64189021a4e07d3361eae548234861
Kyle Chun <kbchun@users.noreply.github.com>
2016-11-19 11:53:10 -0800

    (fix) Fix rejected slack token error
    * (fix) Fix bug regarding OAuth
    
    * (fix) Fix alec's mistakes
    
    * Retrieve user's slack token and save to DB
    
    * Implement Evernote OAuth and refactor OAuth strategies/routes
    
    * (feat) Implement local login and session
    
    * (feat) Save user's slack and evernote tokens to database
    
    * (bug) debug required file error
    
    * (release) V 0.1.1
    
    * (fix) V 0.1.1
    
    * (hotfix) typo in module import
    
    * (hotfix) strict mode heroku issue
    
    * (hotfix) solved allow origin issue
    
    * (test) webhook looging
    
    * (test) testing event api of Slack
    
    * (feat/client) Implement save concoction button and connect oauth for slack and evernote from client to server
    
    * (feat) Node running on Cluster
    
    * (fix) Change local url to heroku url
    
    * (refactor) delted unneeded comments
    
    * (fix) strict mode for oauth
    
    * (feat) implemented delayed worker restart when died
    
    * (test) better insight in event webhook
    
    * (test) testing webhook
    
    * (test) exploring file property
    
    * (test) back to logging full req.body
    
    * (test) listening for messages
    
    * (refactor) created Url file in env to quickly change url for local testing
    
    * (optimization) cluster just running if more than 1 worker possible
    
    * (feat) Retrieve file from Slack using the fileId from webhook
    
    * (feat) Retrieve file from Slack using fileId from webhook
    
    * (cleanup) Add request package
    
    * (bug) File retrieval bug
    
    * (bug) File retrieval bug
    
    * (bug) Another small file retrieval bug
    
    * (bug) Last bug and console log confirming slack obj is valid
    
    * (bug) Last bug I swear
    
    * (bug) Parsed the response from slack
    
    * (bug) woo
    
    * (bug) again
    
    * (bug) Deal with synchronous issues
    
    * (bug) Syntax error
    
    * (bug) Another syntax error
    
    * (bug) Remove parse
    
    * (bug) Console log fileObj from API request
    
    * (bug) Require request-promise
    
    * (bug) Parse object
    
    * (cleanup) Refactor
    
    * (cleanup) Fix merge
    
    * (refactor) Retrieve evernote token from paramObj instead of querying the DB; gitignore bundle.js.map
    
    * (bug) Fix route authentication to properly work
    
    * (feat) Retrieve user's concoctions from server
    
    * (feat) Fetch and render concoctions; implement logic to prompt user to oauth
    
    * (bug) logic for oauth button
    
    * Change url
    
    * Add 'use strict'
    
    * Add error handling in slackController and use correct token
    
    * Fix bug
    
    * Another bug
    
    * Parse fileObj
    
    * Return result
    
    * Add slackAppToken
    
    * Remove console log

--- server/db/controllers/slackController.js
+++ server/db/controllers/slackController.js
@@ -6,6 +6,7 @@ const request = require('request-promise');
 const SlackStrategy = require('passport-slack').Strategy;
 const SLACK_ID = process.env.SLACK_ID || require('../../../env.js').SLACK_ID;
 const SLACK_SECRET = process.env.SLACK_SECRET || require('../../../env.js').SLACK_SECRET;
+const token = process.env.slackAppToken || require('../../../env.js').slackAppToken;
 
 module.exports.Strategy = new SlackStrategy({
   clientID: SLACK_ID,
@@ -19,6 +20,15 @@ module.exports.Strategy = new SlackStrategy({
   });
 });
 
-module.exports.getFile = (slackId, fileId) => User.findOne({ slackId: slackId })
-  .then((user) => request(`https://slack.com/api/files.info?token=${user.slackToken}&file=${fileId}&pretty=1`))
-  .then((fileObj) => JSON.parse(fileObj).file);
+module.exports.getFile = (slackId, fileId) => {
+  return request(`https://slack.com/api/files.info?token=${token}&file=${fileId}&pretty=1`)
+  .then((fileObj) => {
+    fileObj = JSON.parse(fileObj);
+    if (fileObj.ok) {
+      return fileObj.file;
+    } else {
+      throw new Error(fileObj);
+    }
+  })
+  .catch((error) => { console.log('Error in slackCtrl getFile: ', error); });
+};
