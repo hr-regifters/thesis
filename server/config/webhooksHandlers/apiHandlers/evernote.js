1aaffa6e5e9fefa3511e149f38875dca87b10798
Kyle Chun <kbchun5712@gmail.com>
2016-11-22 19:24:21 -0800

    Typo

--- server/config/webhooksHandlers/apiHandlers/evernote.js
+++ server/config/webhooksHandlers/apiHandlers/evernote.js
@@ -46,7 +46,7 @@ module.exports = {
       //if parentNotebook is defined
       if (paramObj.actionParams.parentNotebook) {
         noteStore.listNotebooks(function(err, notebooks) {
-          if (typeof notebook === 'object') {
+          if (typeof notebooks === 'object') {
             // find the guid for the notebook with a name matching 'parentNotebook'
             var notebook = notebooks.filter(function(notebook){ return parentNotebook.toLowerCase() === notebook.name.toLowerCase()});
             console.log('notebook', notebook);
