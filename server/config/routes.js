2e2b64b9dc143234641ee5405b56add9c088a69a
Kyle Chun <kbchun@users.noreply.github.com>
2016-11-22 20:04:24 -0800

    Update routes.js

--- server/config/routes.js
+++ server/config/routes.js
@@ -6,14 +6,10 @@ const checkLogin = require('./utilities/checkLogin');
 // put routers here
 
 module.exports = (app) => {
-  app.use('/api/constructor', concoctionConstructor);
+  app.use('/api/constructor', checkLogin, concoctionConstructor);
   app.use('/api/user', userRouter);
   app.use('/api/oauth', checkLogin, oauthRouter);
   app.use('/api/webhooks', webhookRouter);
-<<<<<<< HEAD
-  // app.use('/loaderio-63505aef1c99acae4eea09e626d6e2fc/', (req, res) => { res.send('loaderio-63505aef1c99acae4eea09e626d6e2fc'); });
-=======
   app.use('/loaderio-63505aef1c99acae4eea09e626d6e2fc/', (req, res) => { res.send('loaderio-63505aef1c99acae4eea09e626d6e2fc'); });
   app.get('/*', (req, res) => { res.redirect('/'); }); // catching gets on invalid routes
->>>>>>> 9baf3577d47e2926a51ac666a73cc17ca3bfe02f
 };
