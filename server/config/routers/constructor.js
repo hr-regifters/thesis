3c50ce0833ff139ac712cb7a4a0253f6ace043ed
Kyle Chun <kbchun@users.noreply.github.com>
2016-11-22 20:03:00 -0800

    Update constructor.js

--- server/config/routers/constructor.js
+++ server/config/routers/constructor.js
@@ -6,9 +6,6 @@ const router = new express.Router();
 router.post('/slack/add', concoctionConstructor.createConcoction);
 // router.post('/slack/query',concoctionConstructor.getSlackEvent);
 router.get('/test', concoctionConstructor.queryConcoctions)
-
-router.post('/slack/add', concoctionConstructor.createSlackTrigger);
-// router.post('/slack/query', concoctionConstructor.getSlackEvent);
 router.post('/changeEnabled', concoctionConstructor.toggleConcoction);
 
 
