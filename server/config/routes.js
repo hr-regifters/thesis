8ed9b29f9738780bad4b5adbd8cd53082dc4a3ee
Kyle Chun <kbchun5712@gmail.com>
2016-11-22 17:11:00 -0800

    (cleanup) Protect oauth routes

--- server/config/routes.js
+++ server/config/routes.js
@@ -2,13 +2,12 @@ const concoctionConstructor = require('./routers/constructor');
 const userRouter = require('./routers/user');
 const oauthRouter = require('./routers/oauth');
 const webhookRouter = require('./routers/webhooks');
-const checkLogin = require('./utilities/checkLogin');
 // put routers here
 
 module.exports = (app) => {
   app.use('/api/constructor', checkLogin, concoctionConstructor);
   app.use('/api/user', userRouter);
-  app.use('/api/oauth', checkLogin, oauthRouter);
+  app.use('/api/oauth', oauthRouter);
   app.use('/api/webhooks', webhookRouter);
   app.use('/loaderio-63505aef1c99acae4eea09e626d6e2fc/', (req, res) => { res.send('loaderio-63505aef1c99acae4eea09e626d6e2fc'); });
   app.get('/*', (req, res) => { res.redirect('/'); }); // catching gets on invalid routes
