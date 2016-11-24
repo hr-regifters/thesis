97397e4443109eb7ca8e508f079f4a5d10b1261c
Kyle Chun <kbchun@users.noreply.github.com>
2016-11-23 17:14:18 -0800

    Update fitbitController.js

--- server/db/controllers/fitbitController.js
+++ server/db/controllers/fitbitController.js
@@ -1,13 +1,12 @@
 const passport = require('passport');
 const currUrl = require('./../../../currUrl');
+
 const FITBIT_ID = process.env.FITBIT_ID || require('../../../env.js').FITBIT_ID;
 const FITBIT_SECRET = process.env.FITBIT_SECRET || require('../../../env.js').FITBIT_SECRET;
+const FitbitStrategy = require( 'passport-fitbit-oauth2' ).FitbitOAuth2Strategy;;
 
-
-var FitbitStrategy = require( 'passport-fitbit-oauth2' ).FitbitOAuth2Strategy;;
-
-module.exports.Strategy = passport.use(new FitbitStrategy({
-    clientID:     FITBIT_ID,
+module.exports.Strategy = new FitbitStrategy({
+    clientID: FITBIT_ID,
     clientSecret: FITBIT_SECRET,
     callbackURL: `${currUrl}/api/oauth/fitbit/callback`
   },
