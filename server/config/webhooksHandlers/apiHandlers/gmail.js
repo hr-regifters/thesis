a7a8cc77c3641585bf38f99d21ebedccf5a5bc73
Kyle Chun <kbchun5712@gmail.com>
2016-11-28 18:44:20 -0800

    Change base64 conversion

--- server/config/webhooksHandlers/apiHandlers/gmail.js
+++ server/config/webhooksHandlers/apiHandlers/gmail.js
@@ -13,8 +13,8 @@ module.exports = {
       let subject = 'Subject: ' + paramObj.actionParams.subject;
       let message = paramObj.actionParams.gmail_text;
       let body = `${recipient}/r/n${userEmail}/r/n${subject}/r/n${message}`
-      let base64Email = new Buffer(body).toString('base64');
-      base64Email = base64Email.replace(/\+/g, '-').replace(/\//g, '_');
+      // let base64Email = new Buffer(body).toString('base64');
+      // base64Email = base64Email.replace(/\+/g, '-').replace(/\//g, '_');
       let token = paramObj.actionToken;
       let options = {
         uri: `https://www.googleapis.com/gmail/v1/users/${userEmail}/messages/send`,
@@ -24,7 +24,7 @@ module.exports = {
           Authorization: `Bearer ${token}`
         },
         json: {
-          'raw': base64Email
+          'raw': window.btoa(body);
         }
       }
       request(options, (err, res, body) => {
