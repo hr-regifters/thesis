b2e239335bb88cb1a9d48efed95b390d92fbc72a
Kyle Chun <kbchun5712@gmail.com>
2016-11-28 19:10:31 -0800

    Fix

--- server/config/webhooksHandlers/apiHandlers/gmail.js
+++ server/config/webhooksHandlers/apiHandlers/gmail.js
@@ -13,8 +13,9 @@ module.exports = {
       let subject = 'Subject: ' + paramObj.actionParams.subject;
       let message = paramObj.actionParams.gmail_text;
       let body = `${recipient}/r/n${userEmail}/r/n${subject}/r/n${message}`
-      let base64Email = new Buffer(body).toString('base64');
-      base64Email = base64Email.replace(/\+/g, '-').replace(/\//g, '_');
+      // let base64Email = new Buffer(body).toString('base64');
+      // base64Email = base64Email.replace(/\+/g, '-').replace(/\//g, '_');
+      body = window.btoa(body);
       let token = paramObj.actionToken;
       let options = {
         uri: `https://www.googleapis.com/gmail/v1/users/${userEmail}/messages/send`,
@@ -24,12 +25,12 @@ module.exports = {
           Authorization: `Bearer ${token}`
         },
         json: {
-          'raw': base64Email
+          'raw': body
         }
       }
       request(options, (err, res, body) => {
-        if (err) {
-          console.log('error', err);
+        if (body.error) {
+          console.log('error', body.err);
         } else {
           console.log(body);
           console.log('Successfully sent email');
