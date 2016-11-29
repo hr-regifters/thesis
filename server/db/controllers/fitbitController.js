07694e450295758979e5d15ab96463550aa5595b
Ben Hesseldieck <b.hesseldieck@gmail.com>
2016-11-29 11:43:20 -0800

    merge prepare

--- server/db/controllers/fitbitController.js
+++ server/db/controllers/fitbitController.js
@@ -1,16 +1,16 @@
-// const passport = require('passport');
-// const currUrl = require('./../../../currUrl');
+const passport = require('passport');
+const currUrl = require('./../../../currUrl');
 
-// const FitbitStrategy = require( 'passport-fitbit-oauth2' ).FitbitOAuth2Strategy;;
-// const FITBIT_ID = process.env.FITBIT_ID || require('../../../env.js').FITBIT_ID;
-// const FITBIT_SECRET = process.env.FITBIT_SECRET || require('../../../env.js').FITBIT_SECRET;
+const FitbitStrategy = require( 'passport-fitbit-oauth2' ).FitbitOAuth2Strategy;;
+const FITBIT_ID = process.env.FITBIT_ID || require('../../../env.js').FITBIT_ID;
+const FITBIT_SECRET = process.env.FITBIT_SECRET || require('../../../env.js').FITBIT_SECRET;
 
-// module.exports.Strategy = new FitbitStrategy({
-//   clientID: FITBIT_ID,
-//   clientSecret: FITBIT_SECRET,
-//   callbackURL: `${currUrl}/api/oauth/fitbit/callback`
-// }, (accessToken, refreshToken, profile, done) => {
-//   process.nextTick(() => {
-//     return done(null, accessToken);
-//   });
-// });
+module.exports.Strategy = new FitbitStrategy({
+  clientID: FITBIT_ID,
+  clientSecret: FITBIT_SECRET,
+  callbackURL: `${currUrl}/api/oauth/fitbit/callback`
+}, (accessToken, refreshToken, profile, done) => {
+  process.nextTick(() => {
+    return done(null, accessToken);
+  });
+});
