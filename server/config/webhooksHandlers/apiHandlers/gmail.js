d7611c3e46476dea766dcda4e609c769df0cd3e7
Kyle Chun <kbchun5712@gmail.com>
2016-11-28 19:52:57 -0800

    fix

--- server/config/webhooksHandlers/apiHandlers/gmail.js
+++ server/config/webhooksHandlers/apiHandlers/gmail.js
@@ -8,18 +8,16 @@ module.exports = {
   },
   actions: {
     send_email: (paramObj) => {
-      let userEmail = paramObj.actionParams.email;
-      let sender = 'From: ' + userEmail;
-      // let replyTo = 'Reply-To: ' + userEmail;
+      let userEmail = 'From: ' + paramObj.actionParams.email;
       let recipient = 'To: ' + paramObj.actionParams.recipient;
       let subject = 'Subject: ' + paramObj.actionParams.subject;
       let message = paramObj.actionParams.gmail_text;
-      let body = `${recipient}\r\n${sender}\r\n${subject}\r\n${message}`
+      let body = `${recipient}\r\n${userEmail}\r\n${subject}\r\n${message}`
       let base64Email = new Buffer(body).toString('base64');
       base64Email = base64Email.replace(/\+/g, '-').replace(/\//g, '_');
       let token = paramObj.actionToken;
       let options = {
-        uri: `https://www.googleapis.com/gmail/v1/users/${userEmail}/messages/send`,
+        uri: `https://www.googleapis.com/gmail/v1/users/${paramObj.actionParams.email}/messages/send`,
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
