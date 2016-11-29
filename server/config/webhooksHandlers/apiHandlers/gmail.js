63da998330694cf5218e43dca191e8d20adb5c74
Kyle Chun <kbchun5712@gmail.com>
2016-11-28 17:44:31 -0800

    Fix gmail action

--- server/config/webhooksHandlers/apiHandlers/gmail.js
+++ server/config/webhooksHandlers/apiHandlers/gmail.js
@@ -11,12 +11,16 @@ module.exports = {
       console.log('sending email')
       let email = 'kbchun5712@gmail.com';
       let token = 'ya29.CjukA9v_WzCks8A4OjwKdNODgwH56io7CKnYu16-kxD4g4KQrK92vy2fcv0wZufJhwFY_cYvzmX_bFG2NQ';
-      request.post(`https://www.googleapis.com/gmail/v1/users/${email}/messages/send?access_token${token}`, {
-        "userId": "me",
-        "resource": {
-          "raw": "VG86IGtiY2h1bjU3MTJAZ21haWwuY29tDQpGcm9tOiBrYmNodW41NzEyQGdtYWlsLmNvbQ0KSGVsbG8="
+      let options = {
+        uri: `https://www.googleapis.com/gmail/v1/users/${email}/messages/send`,
+        headers: {
+          Authorization: `Bearer ${token}`
+        },
+        body: {
+          'raw': 'VG86IGtiY2h1bjU3MTJAZ21haWwuY29tDQpGcm9tOiBrYmNodW41NzEyQGdtYWlsLmNvbQ0KSGVsbG8='
         }
-      }, (err, res, body) => {
+      }
+      request.post(options, (err, res, body) => {
         console.log('error', err);
         console.log('res', res);
         console.log('body', body);
