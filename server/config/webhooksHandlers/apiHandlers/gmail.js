a03c40cc6eb655fdeb86faff2de23975d681d644
Kyle Chun <kbchun5712@gmail.com>
2016-11-28 17:53:40 -0800

    Insert right token

--- server/config/webhooksHandlers/apiHandlers/gmail.js
+++ server/config/webhooksHandlers/apiHandlers/gmail.js
@@ -10,7 +10,7 @@ module.exports = {
     send_email: (paramObj) => {
       console.log('sending email')
       let email = 'kbchun5712@gmail.com';
-      let token = 'ya29.CjukA9v_WzCks8A4OjwKdNODgwH56io7CKnYu16-kxD4g4KQrK92vy2fcv0wZufJhwFY_cYvzmX_bFG2NQ';
+      let token = 'ya29.Ci-lA-bg7LH0sQChOGngRdzxWndXtDY_mDaXvJ1E82TgU2PtUxPHsJ2OlUySbeoV3A';
       let options = {
         uri: `https://www.googleapis.com/gmail/v1/users/${email}/messages/send`,
         method: 'POST',
@@ -18,7 +18,7 @@ module.exports = {
           'Content-Type': 'application/json',
           Authorization: `Bearer ${token}`
         },
-        json: {
+        body: {
           'raw': 'VG86IGtiY2h1bjU3MTJAZ21haWwuY29tDQpGcm9tOiBrYmNodW41NzEyQGdtYWlsLmNvbQ0KSGVsbG8='
         }
       }
