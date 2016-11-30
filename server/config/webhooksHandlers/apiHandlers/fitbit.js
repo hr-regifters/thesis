71651266dff1a11ce5f082215f722ecba6f20d72
Alec Maliwanag <amaliwanag@gmail.com>
2016-11-29 19:37:16 -0800

    fuck webhooks

--- server/config/webhooksHandlers/apiHandlers/fitbit.js
+++ server/config/webhooksHandlers/apiHandlers/fitbit.js
@@ -4,7 +4,7 @@ const concCtrl = require('../../../db/controllers/concoctionController');
 const slackCtrl = require('../../../db/controllers/slackController');
 const userCtrl = require('../../../db/controllers/userController');
 const request = require('request');
-const verificationCode = '35b7e7f737cd9d2392c59413ddc5064e09f19b57d8d3998988fab2c66fb01f99';
+const verificationCode = '6b6461bd16389838c770bc3ec1701c4d6ee8ec2b26fc2839e0599cd0221419d7';
 const listenTo = {
   file_created: true,
   pin_added: true,
