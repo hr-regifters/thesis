5975641c548f38069b8d5a6ba0dd54cd06375f3d
Dylan Larrabee <dylan.r.larrabee@gmail.com>
2016-12-01 11:46:49 -0800

    Modify oauth redirect to /app route

--- server/config/middleware.js
+++ server/config/middleware.js
@@ -5,7 +5,7 @@ const passport = require('../db/controllers/passportController');
 module.exports = (app, express) => {
   app.use(bodyParser.json()); // potentially add inflate true if a webhook compresses data
   app.use(bodyParser.urlencoded({ extended: true }));
-  app.use('/signup', express.static(path.join(__dirname, './../../client/dist')));
-  app.use(express.static(path.join(__dirname, './../../client/landingPage')));
+  app.use('/app', express.static(path.join(__dirname, './../../client/dist'))); // the '/app' route
+  app.use(express.static(path.join(__dirname, './../../client/landingPage'))); // the main '/' route
   passport(app);
 };
