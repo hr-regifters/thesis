28ec91f0d96a2d0f80f70f2c57557d5f5ab0f89d
Kyle Chun <kbchun5712@gmail.com>
2016-12-01 12:47:00 -0800

    Add console logs

--- server/config/webhooksHandlers/apiHandlers/strava.js
+++ server/config/webhooksHandlers/apiHandlers/strava.js
@@ -17,7 +17,6 @@ module.exports = {
       actionToken: ''
     };
     concCtrl.getConcoctions('strava', 'activity_logged', req.body['ownerId']).then((concoctionList) => {
-      console.log(concoctionList.rows, 'concoctionList.rows');
       let concoctions = concoctionList.rows.filter((concoction) => concoction.enable === true);
       let options = {
         url: `https://www.strava.com/api/v3/activities/${req.body['object_id']}`,
@@ -37,12 +36,14 @@ module.exports = {
             let activity = JSON.parse(concoction.triggerparams).param['strava_activity'].toLowerCase();
             return body.type === activity;
           });
+          console.log('concoctions', concoctions);
           // look at each individual concoction
           async.each((concoctions), (concoction, callback) => {
             let stravaData = JSON.parse(body);
             stravaReqObj.actionParams = JSON.parse(concoction.actionparams);
             stravaReqObj.actionToken = concoction.actiontoken;
-
+            console.log('concoction', concoction);
+            console.log('strava data', stravaData);
             // check which action apis we're dealing with and what corresponding action
             if (concoction.actionapi === 'googleSheets' && concoction.actionevent === 'create_sheet') {
               let sheetData = {
@@ -58,6 +59,7 @@ module.exports = {
                 max_speed: data.max_speed,
                 calories: data.calories
               };
+              console.log('sheet data', sheetData);
               stravaReqObj.data = sheetData;
               webhooksHandler[`${concoction.actionapi}Action`][concoction.actionevent](stravaReqObj);
               callback();
